ad383293f9b636696142629d2eefa03ca7a0da97
Vjze <z123166263@gmail.com>
2025-10-22 20:16:44 +0800

    Update login_view.rs

--- src/login_view.rs
+++ src/login_view.rs
@@ -9,7 +9,6 @@ live_design! {
     use link::theme::*;
     use link::shaders::*;
     use link::widgets::*;
-
     use crate::shared::styles::*;
     use crate::shared::widgets::*;
     use crate::shared::modal::*;
