e78a74ba2ce135e37ee013b2313b07517654c0ec
Vjze <z123166263@gmail.com>
2025-10-29 22:33:36 +0800

    ....

--- src/utils/sql.rs
+++ src/utils/sql.rs
@@ -4,7 +4,8 @@ use sqlx_oldapi::{MssqlPool, Row};
 
 pub async fn client() -> anyhow::Result<MssqlPool, MyError> {
     // let database_url = "mssql://yytest:yytest@localhost:1433/BOSAautotestDB";
-    let database_url = "mssql://sa:Wjz142857.@localhost:1433/BOSAautotestDB";
+    let database_url = "mssql://yytest:yytest@192.3.85.240:6001/BOSAautotestDB?trust_server_certificate=true&encrypt=true";
+    // let database_url = "mssql://sa:Wjz142857.@localhost:1433/BOSAautotestDB";
     let pool = MssqlPoolOptions::new()
         // 设置最大连接数：推荐 10-25
         .max_connections(100)
